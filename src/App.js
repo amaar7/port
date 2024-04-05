74a8a5d9193b29e27b9e71479d34389f8d6aea4a
amaar <amaar.ahmednur@student.moringaschool.com>
2024-04-05 23:01:12 +0300

    added about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Hero from "./componets/Hero";
 import Navbar from "./componets/Navbar";
 import Skills from "./componets/Skills";
+import About from "./componets/About";
+
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
       <Skills/>
+      <About/>
     </div>
   );
 }
